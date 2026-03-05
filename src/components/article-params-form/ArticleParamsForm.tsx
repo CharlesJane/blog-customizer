9a9c5efb879c1b026781380da66c380f27f1f122
Kate Polenova <double.none@yandex.ru>
2026-03-05 18:26:26 +0300

    Добавлен функционал работы селекторов, вынесено запоминание выбранного селекта в handleChange, нужно разобраться с отображением стилей, сломанные обертки

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -5,14 +5,33 @@ import { Button } from 'src/ui/button';
 import { Select } from 'src/ui/select';
 import { Separator } from 'src/ui/separator';
 import { RadioGroup } from 'src/ui/radio-group';
+import {
+	backgroundColors,
+	contentWidthArr,
+	fontColors,
+	fontFamilyOptions,
+	defaultArticleState,
+	ArticleStateType,
+	fontSizeOptions,
+	OptionType,
+} from 'src/constants/articleProps';
 
 import styles from './ArticleParamsForm.module.scss';
 import { useEffect, useState } from 'react';
 
 export const ArticleParamsForm = () => {
 	const [isParamsFormOpen, setIsParamsFormOpen] = useState(false);
+	const [articleState, setArticleState] =
+		useState<ArticleStateType>(defaultArticleState);
 
 	const toggleParamsForm = () => setIsParamsFormOpen((prev) => !prev);
+	const handleChange =
+		(fieldName: keyof ArticleStateType) => (selectedOption: OptionType) => {
+			setArticleState((prev) => ({
+				...prev,
+				[fieldName]: selectedOption,
+			}));
+		};
 
 	useEffect(() => {
 		const handleClickOutside = (evt: MouseEvent) => {
@@ -40,12 +59,33 @@ export const ArticleParamsForm = () => {
 				aria-hidden={!isParamsFormOpen}>
 				<form className={styles.form}>
 					<div className={styles.bottomContainer}>
-						<Select selected={} options={} />
-						<RadioGroup name='name' options={} selected={} title=' ' />
-						<Select selected={} options={} />
+						<Select
+							selected={articleState.fontFamilyOption}
+							options={fontFamilyOptions}
+							onChange={handleChange('fontFamilyOption')}
+						/>
+						<RadioGroup
+							name='name'
+							options={fontSizeOptions}
+							selected={articleState.fontSizeOption}
+							title=' '
+						/>
+						<Select
+							selected={articleState.fontColor}
+							options={fontColors}
+							onChange={handleChange('fontColor')}
+						/>
 						<Separator />
-						<Select selected={} options={} />
-						<Select selected={} options={} />
+						<Select
+							selected={articleState.backgroundColor}
+							options={backgroundColors}
+							onChange={handleChange('backgroundColor')}
+						/>
+						<Select
+							selected={articleState.contentWidth}
+							options={contentWidthArr}
+							onChange={handleChange('contentWidth')}
+						/>
 						<Button title='Сбросить' htmlType='reset' type='clear' />
 						<Button title='Применить' htmlType='submit' type='apply' />
 					</div>
